95e38c94164573dfa23a7219f4ca356af2a2a0d6
chatact135-wq <chatact135@gmail.com>
2026-09-16 11:57:17 +0400

    Update views.py

--- store/views.py
+++ store/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
-from .models import Category, Product, PromoCode
+from .models import Category, Product, PromoCode, HeroSlide
 
 def home_view(request):
     categories = Category.objects.all()
     products = Product.objects.all()
+    slides = HeroSlide.objects.all()
     promos = PromoCode.objects.filter(is_active=True)
     
-    # Convert promos into a dictionary for easy JS lookup e.g. {'SARA10': 10}
     promo_dict = {p.code.upper(): p.discount_percentage for p in promos}
 
     context = {
         'categories': categories,
         'products': products,
+        'slides': slides,
         'promo_dict': promo_dict,
     }
     return render(request, 'store/index.html', context)
