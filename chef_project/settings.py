c4fb93b269213e245e425f106b226d5ba6d9d463
chatact135-wq <chatact135@gmail.com>
2026-09-16 09:46:07 +0400

    Update settings.py

--- chef_project/settings.py
+++ chef_project/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-change-me-in-production-environment")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ['https://chefsara-production.up.railway.app']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
