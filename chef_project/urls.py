670b35b105776474b0570cda67e8909b6451d895
chatact135-wq <chatact135@gmail.com>
2026-09-16 13:15:34 +0400

    Update urls.py

--- chef_project/urls.py
+++ chef_project/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from store.views import home_view
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
+    path('', include('store.urls')), # or your store app include
 ]
+
+# Add this line to serve uploaded media files during development/production
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
