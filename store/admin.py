2c19f1b91d96412bac50660e384ff3f022934957
chatact135-wq <chatact135@gmail.com>
2026-09-16 10:33:48 +0400

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, PromoCode
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,3 +11,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'inventory_stock', 'is_bestseller')
     list_filter = ('category', 'is_bestseller')
     search_fields = ('name', 'description')
+
+@admin.register(PromoCode)
+class PromoCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount_percentage', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('code',)
