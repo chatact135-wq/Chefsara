832b010b72d75beace9bb13310285bd6622c4ca4
chatact135-wq <chatact135@gmail.com>
2026-09-19 14:07:38 +0300

    Update models.py

--- store/models.py
+++ store/models.py
@@ -33,8 +33,9 @@ class Product(models.Model):
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     subtitle = models.CharField(max_length=200)
-    description = models.TextField()
+    description = RichTextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory_stock = models.PositiveIntegerField(default=10)
     is_bestseller = models.BooleanField(default=False)
@@ -42,6 +43,14 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name} (AED {self.price})"
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ImageField(upload_to='product_gallery/')
+    alt_text = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
 
 class HeroSlide(models.Model):
     SLIDE_GRAPHICS = [
