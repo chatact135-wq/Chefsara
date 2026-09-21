deed6a3a0f770e2d35978258cfdfbd2023405ddb
chatact135-wq <chatact135@gmail.com>
2026-09-21 10:00:22 +0400

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('product/<slug:product_slug>/', views.product_detail_view, name='product_detail'),
+    path('category/<slug:category_slug>/', views.category_detail_view, name='category_detail'),
 ]
