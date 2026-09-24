0c19da14c5ce372a50ff46b73fbb04635bbaa936
chatact135-wq <chatact135@gmail.com>
2026-09-24 09:05:38 +0400

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,19 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from django.contrib.sitemaps.views import sitemap
+from store.sitemaps import StaticViewSitemap, CategorySitemap, ProductSitemap
+from store import views  # Corrected import from the store app
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    'categories': CategorySitemap,
+    'products': ProductSitemap,
+}
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home_view, name='home'),
     path('product/<slug:product_slug>/', views.product_detail_view, name='product_detail'),
     path('category/<slug:category_slug>/', views.category_detail_view, name='category_detail'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
